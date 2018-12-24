b2f45ae78ef5d3702dc3ff8370a732592153918f
David Dai <stdioa@163.com>
2018-12-24 22:21:25 +0800

    Site updated: 2018-12-24 22:21:07

--- js/application.js
+++ js/application.js
@@ -1,3 +1,3 @@
-// build time:Sun Dec 09 2018 20:37:31 GMT+0800 (中国标准时间)
+// build time:Mon Dec 24 2018 22:21:05 GMT+0800 (中国标准时间)
 $(function(){$('[data-toggle="tooltip"]').tooltip();if(typeof $.fn.slimScroll!="undefined"){$(".sidebar .slimContent").slimScroll({height:$(window).height(),color:"rgba(0,0,0,0.15)",size:"5px",position:"right"})}$("#collapseToc").on("shown.bs.collapse",function(){if(typeof $.fn.slimScroll!="undefined"){$(".sidebar .slimContent").slimScroll().on("slimscroll")}});$(".geopattern").each(function(){$(this).geopattern($(this).data("pattern-id"))});var t=$("#nav-main").okayNav({swipe_enabled:false});$("[data-stick-bottom]").keepInView({fixed:false,parentClass:"has-sticky",customClass:"sticky",trigger:"bottom",zindex:42,edgeOffset:0});$("[data-stick-top]").keepInView({fixed:true,parentClass:"has-sticky",customClass:"sticky",trigger:"top",zindex:42,edgeOffset:0})});
 //rebuild by neat 
